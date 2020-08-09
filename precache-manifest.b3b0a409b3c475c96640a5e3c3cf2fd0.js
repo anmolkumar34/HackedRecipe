a65826ad4ef5b27ec1ba04e08a569c431c9635f3
anmol kumar <anmolkumar34@gmail.com>
2020-08-09 22:32:54 +0530

    Updates

---
+++ precache-manifest.b3b0a409b3c475c96640a5e3c3cf2fd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52a3a6b4671de3e4a8baee85caa5970",
+    "url": "/HackedRecipe/index.html"
+  },
+  {
+    "revision": "8331733c9b6bc8c3c711",
+    "url": "/HackedRecipe/static/css/main.c6ca5b21.chunk.css"
+  },
+  {
+    "revision": "bcd62ee511347a6507a7",
+    "url": "/HackedRecipe/static/js/2.ea3ccbf8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/HackedRecipe/static/js/2.ea3ccbf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8331733c9b6bc8c3c711",
+    "url": "/HackedRecipe/static/js/main.fb455493.chunk.js"
+  },
+  {
+    "revision": "bd0d3510a9a70d3a45d7",
+    "url": "/HackedRecipe/static/js/runtime-main.3fe7de00.js"
+  }
+]);
